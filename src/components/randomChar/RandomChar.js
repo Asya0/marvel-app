e506f429bf5e45b45093f2f758ceaa03dbecf7d9
Asya0 <anya.gerasimenko763@mail.ru>
2025-08-19 00:20:56 +0600

    feat: добавлен спиннер и компонент ошибки при отображении случайного персонажа

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
+import Spinner from "../../components/spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelServices from "../../services/MarvelService";
 
 class RandomChar extends Component {
@@ -12,48 +14,41 @@ class RandomChar extends Component {
 
   state = {
     char: {},
+    loading: true,
+    error: false,
   };
 
   marvelService = new MarvelServices(); // создание нового свойства внутри класса RandomChar
 
   onCharLoaded = (char) => {
-    this.setState({ char });
+    this.setState({ char, loading: false });
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
   };
 
   //метод, который будет обращаться к серверу, будет получать данные и записывать в стейт
   //исп. стрелочную функцию чтобы не терять контекст
   updateChar = () => {
     const id = Math.floor(Math.random() * (20 - 1) + 1);
-    this.marvelService.getCharacter(id).then(this.onCharLoaded);
+    this.marvelService
+      .getCharacter(id)
+      .then(this.onCharLoaded)
+      .catch(this.onError);
   };
 
   render() {
-    const {
-      char: { name, description, thumbnail, homepage, wiki },
-    } = this.state;
+    const { char, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View char={char} /> : null;
+
     return (
       <div className="randomchar">
-        <div className="randomchar__block">
-          <img
-            src={thumbnail}
-            alt="Random character"
-            className="randomchar__img"
-          />
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">
-              {description ? description : "Данных про этого персонажа нет"}
-            </p>
-            <div className="randomchar__btns">
-              <a href={homepage} className="button button__main">
-                <div className="inner">homepage</div>
-              </a>
-              <a href={wiki} className="button button__secondary">
-                <div className="inner">wiki</div>
-              </a>
-            </div>
-          </div>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
         <div className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!
@@ -70,5 +65,31 @@ class RandomChar extends Component {
     );
   }
 }
+
+// простой рендерующий компонент
+// т.к нет логики -> просто получает объект с данными, принимает и возвращает верстку
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki } = char;
+
+  return (
+    <div className="randomchar__block">
+      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <div className="randomchar__info">
+        <p className="randomchar__name">{name}</p>
+        <p className="randomchar__descr">
+          {description ? description : "Данных про этого персонажа нет"}
+        </p>
+        <div className="randomchar__btns">
+          <a href={homepage} className="button button__main">
+            <div className="inner">homepage</div>
+          </a>
+          <a href={wiki} className="button button__secondary">
+            <div className="inner">wiki</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RandomChar;
